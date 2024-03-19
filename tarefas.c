d9accb10751fbeda43005157559308fe5bbeea5f
Guilherme Meireles <guizinho6030@gmail.com>
2024-03-19 23:41:52 +0000

    funções placebo dos indices adicionadas

--- tarefas.c
+++ tarefas.c
@@ -1,2 +1,23 @@
+#include "tarefas.h"
 #include <stdio.h>
-#include <tarefas.h>
+  
+int criar(Tarefa tarefas[], int *position){
+  printf("Função de criar tarefa\n")
+    return 0;
+}
+int deletar(Tarefa tarefas[], int *position){
+  printf("Função de deletar tarefa\n")
+    return 0;
+}
+int listar(Tarefa tarefas[], int position){
+  printf("Função de listar tarefa\n")
+    return 0;
+}
+int salvar(Tarefa tarefas[], int total, int position){
+  printf("Função de salvar tarefa\n")
+    return 0;
+}
+int carregar(Tarefa tarefas[], int total, int position){
+  printf("Função de carregar tarefa\n")
+    return 0;
+}
