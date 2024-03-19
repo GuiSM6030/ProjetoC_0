ded28cf185bbc4d7fbea9ae17c3033a6b71e887b
Guilherme Meireles <guizinho6030@gmail.com>
2024-03-19 23:37:05 +0000

    a partir da linha 12, add de indices

--- tarefas.h
+++ tarefas.h
@@ -8,3 +8,9 @@ typedef struct tarefa{
 }
 
   Tarefa;
+
+int criar(Tarefa tarefas[], int *position);
+int deletar(Tarefa tarefas[], int *position);
+int listar(Tarefa tarefas[], int position);
+int salvar(Tarefa tarefas[], int total, int position);
+int carregar(Tarefa tarefas[], int total, int position);
